a74df6801f60e5e8660d9db88f56e0193208e2be
ruiz <ruizhuillca@gmail.com>
2020-04-24 17:56:17 -0500

    Intenciones lanzando a una app especifica, casos de whatsapp, navegacion com maps y wase

--- app/src/main/java/com/example/mitema2online/Intenciones.java
+++ app/src/main/java/com/example/mitema2online/Intenciones.java
@@ -1,7 +1,9 @@
 package com.example.mitema2online;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -67,5 +69,47 @@ public class Intenciones  extends AppCompatActivity {
         }
 
 
+    }
+
+    public void navegacion(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setMessage("Seleccione la aplicación");
+        builder.setTitle("Escoja entre Waze o Google Maps");
+        builder.setNeutralButton("Google Map", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Uri gmmIntentUri;
+                gmmIntentUri = Uri.parse("google.navigation:q=" + -18.013739816 + "," + -70.2510593169);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(mapIntent);
+                else
+                    Toast.makeText(Intenciones.this, "Maps no esta instalado", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("Waze", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Uri gmmIntentUri;
+                gmmIntentUri = Uri.parse("waze://?ll=" + -18.013739816 + "," + -70.2510593169 + "&navigate=yes");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.waze");
+                if (mapIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(mapIntent);
+                else
+                    Toast.makeText(Intenciones.this, "Waze no esta instalado", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setPositiveButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                return;
+            }
+        });
+        builder.show();
     }
 }
